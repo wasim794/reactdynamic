c4fbc65bb128ebe7cfa845918852bbaa7038a581
wasim794 <ashish.bhagat@inferrix.com>
2020-03-06 15:04:15 +0530

    file chnage here

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,6 +16,17 @@ class Home extends Component {
       itemsthree: [],
       ress:''
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    
+  }
+
+
+  handleClick = () => {
+    console.log('hello');
+  }
+  handleKeyPress= () =>{
+    console.log('hello');
   }
 
 
@@ -72,7 +83,7 @@ class Home extends Component {
         )
 
 
-        // third api used 
+        // third api used
 
 
          // second api call HERE
@@ -155,7 +166,7 @@ class Home extends Component {
                   <div className="advanced-search">
                     <button type="button" className="category-btn">All Categories</button>
                     <div className="input-group">
-                      <input type="text" placeholder="What do you need?" />
+                      <input type="text" placeholder="What do you need?" onKeyPress={this.handleKeyPress} />
                       <button type="button"><i className="ti-search" /></button>
                     </div>
                   </div>
@@ -293,7 +304,7 @@ class Home extends Component {
                     <h1>Black friday</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
                       incididunt ut labore et dolore</p>
-                    <a href="#" className="primary-btn">Shop Now</a>
+                    <a href="#"   className="primary-btn" onClick={this.handleClick}>Shop Now</a>
                   </div>
                 </div>
                 <div className="off-card">
@@ -310,7 +321,7 @@ class Home extends Component {
           <div className="container-fluid">
             <div className="row">
             {this.state.itemstwo.map(item => (
-              <div className="col-lg-4">
+              <div className="col-lg-3">
                 <div className="single-banner">
                 <img src={item.ImageName} alt={item.ImageType}/>
                   <div className="inner-text">
@@ -319,140 +330,14 @@ class Home extends Component {
                 </div>
               </div>
                 ))}
-              <div className="col-lg-4">
-                <div className="single-banner">
-                  <img src="img/banner-2.jpg" alt="" />
-                  <div className="inner-text">
-                    <h4>Women’s</h4>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4">
-                <div className="single-banner">
-                  <img src="img/banner-3.jpg" alt="" />
-                  <div className="inner-text">
-                    <h4>Kid’s</h4>
-                  </div>
-                </div>
-              </div>
+
             </div>
           </div>
         </div>
         {/* Banner Section End */}
         {/* Women Banner Section Begin */}
         <section className="women-banner spad">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-lg-3">
-                <div className="product-large set-bg" data-setbg="img/products/women-large.jpg">
-                  <h2>Women’s</h2>
-                  <a href="#">Discover More</a>
-                </div>
-              </div>
-              <div className="col-lg-8 offset-lg-1">
-                <div className="filter-control">
-                  <ul>
-                    <li>Clothings</li>
-                    <li>HandBag</li>
-                    <li>Shoes</li>
-                    <li>Accessories</li>
-                  </ul>
-                </div>
-                <div className="product-slider owl-carousel">
-                  <div className="product-item">
-                    <div className="pi-pic">
-                      <img src="img/products/women-1.jpg" alt="" />
-                      <div className="sale">Sale</div>
-                      <div className="icon">
-                        <i className="icon_heart_alt" />
-                      </div>
-                      <ul>
-                        <li className="w-icon active"><a href="#"><i className="icon_bag_alt" /></a></li>
-                        <li className="quick-view"><a href="#">+ Quick View</a></li>
-                        <li className="w-icon"><a href="#"><i className="fa fa-random" /></a></li>
-                      </ul>
-                    </div>
-                    <div className="pi-text">
-                      <div className="catagory-name">Coat</div>
-                      <a href="#">
-                        <h5>Pure Pineapple</h5>
-                      </a>
-                      <div className="product-price">
-                        $14.00
-                        <span>$35.00</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="product-item">
-                    <div className="pi-pic">
-                      <img src="img/products/women-2.jpg" alt="" />
-                      <div className="icon">
-                        <i className="icon_heart_alt" />
-                      </div>
-                      <ul>
-                        <li className="w-icon active"><a href="#"><i className="icon_bag_alt" /></a></li>
-                        <li className="quick-view"><a href="#">+ Quick View</a></li>
-                        <li className="w-icon"><a href="#"><i className="fa fa-random" /></a></li>
-                      </ul>
-                    </div>
-                    <div className="pi-text">
-                      <div className="catagory-name">Shoes</div>
-                      <a href="#">
-                        <h5>Guangzhou sweater</h5>
-                      </a>
-                      <div className="product-price">
-                        $13.00
-                      </div>
-                    </div>
-                  </div>
-                  <div className="product-item">
-                    <div className="pi-pic">
-                      <img src="img/products/women-3.jpg" alt="" />
-                      <div className="icon">
-                        <i className="icon_heart_alt" />
-                      </div>
-                      <ul>
-                        <li className="w-icon active"><a href="#"><i className="icon_bag_alt" /></a></li>
-                        <li className="quick-view"><a href="#">+ Quick View</a></li>
-                        <li className="w-icon"><a href="#"><i className="fa fa-random" /></a></li>
-                      </ul>
-                    </div>
-                    <div className="pi-text">
-                      <div className="catagory-name">Towel</div>
-                      <a href="#">
-                        <h5>Pure Pineapple</h5>
-                      </a>
-                      <div className="product-price">
-                        $34.00
-                      </div>
-                    </div>
-                  </div>
-                  <div className="product-item">
-                    <div className="pi-pic">
-                      <img src="img/products/women-4.jpg" alt="" />
-                      <div className="icon">
-                        <i className="icon_heart_alt" />
-                      </div>
-                      <ul>
-                        <li className="w-icon active"><a href="#"><i className="icon_bag_alt" /></a></li>
-                        <li className="quick-view"><a href="#">+ Quick View</a></li>
-                        <li className="w-icon"><a href="#"><i className="fa fa-random" /></a></li>
-                      </ul>
-                    </div>
-                    <div className="pi-text">
-                      <div className="catagory-name">Towel</div>
-                      <a href="#">
-                        <h5>Converse Shoes</h5>
-                      </a>
-                      <div className="product-price">
-                        $34.00
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+
         </section>
         {/* Women Banner Section End */}
         {/* Deal Of The Week Section Begin*/}
